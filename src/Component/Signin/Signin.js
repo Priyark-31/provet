acf203aef343d31f2af8cc8e767ec48274176694
Elakkiya-tech <67002119+Elakkiya-tech@users.noreply.github.com>
2021-07-19 12:46:28 +0530

    update

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -12,7 +12,7 @@ const Signin =() =>{
     const handleChange = (event, newValue) => {
 		setValue(newValue);
 	};
-    const paperStyle={width:'570px', margin:'15px auto' ,height:'700px', backgroundColor:'#77ACF1'}
+    const paperStyle={width:'570px', margin:'15px auto' ,height:'700px', backgroundColor:'#CDF0EA'}
     function TabPanel(props) {
 		const { children, value, index, ...other } = props;
 
